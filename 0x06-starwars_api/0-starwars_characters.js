5a4b6a8070fc59148594f06c0504f6204534c308
bayzman <bassmaninno102@gmail.com>
2024-11-21 13:42:14 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 /**
- * Prints all characters of a Star Wars movie 
+ * Prints all characters of a Star Wars movie
  */
 
 const request = require('request');
@@ -13,14 +13,20 @@ request(url, function (error, response, body) {
     console.log(error);
   } else {
     const data = JSON.parse(body);
-    for (const character of data.characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    const characters = data.characters;
+    printCharacters(characters, 0); // Start printing characters from index 0
   }
 });
+
+function printCharacters(characters, index) {
+  if (index >= characters.length) return; // Stop when all characters have been printed
+
+  request(characters[index], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1); // Recursively print the next character
+    }
+  });
+}
