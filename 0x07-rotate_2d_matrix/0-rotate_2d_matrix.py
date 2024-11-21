126ff70fa7b98cd28de6e5a70f4fabf17c2904ca
bayzman <bassmaninno102@gmail.com>
2024-11-21 12:40:40 +0300

    Rotate 2D Matrix Clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,10 +2,8 @@
 
 """ Rotate a 2D square matrix 90 degrees clockwise """
 
-from typing import List
 
-
-def rotate_2d_matrix(matrix: List[List]) -> None:
+def rotate_2d_matrix(matrix) -> None:
     """" Rotate matrix 90 degrees clockwise """
     rows = len(matrix)
 
